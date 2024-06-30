a4ee2842531e43768d64e54310be91aa422b5cbd
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-06-30 18:29:39 -0500

    added navbar logo

---
+++ airbnb_clone/app/components/container.tsx
@@ -0,0 +1,13 @@
+interface ContainerProps {
+    children: React.ReactNode;
+}   
+
+const Container: React.FC<ContainerProps> = ({children}) => {
+    return(
+        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-20 sm:px-20 px-4">
+            {children}
+        </div>
+    )
+};
+
+export default Container;
