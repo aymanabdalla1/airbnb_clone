83e3c967840c47a4eebea031b0bb1874b3b26b7c
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-07-04 17:52:59 -0500

    finished input component

--- airbnb_clone/app/components/modals/registerModal.tsx
+++ airbnb_clone/app/components/modals/registerModal.tsx
@@ -4,6 +4,8 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle} from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import {FieldValues, SubmitHandler, useForm} from 'react-hook-form';
+import Heading from '../heading';
+import Input from '../inputs/input';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './modal';
@@ -34,6 +36,39 @@ const RegisterModal = () => {
         })
     }
 
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading 
+            title="Welcome to Airbnb"
+            subtitle="Create an account!"/>
+                <Input
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required 
+                />
+                <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required 
+                />
+                <Input
+                id="passowrd"
+                type="password"
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required 
+                />
+        </div>
+    )
+
     return(
             <Modal
             disabled={isLoading}
@@ -42,6 +77,7 @@ const RegisterModal = () => {
             actionLabel="Continue"
             onClose={registarModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
             />
     )
 }
